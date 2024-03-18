0e14fd82e386eda4e92ea7b6b7ce2919f05e19fb
Chayel J Heinsen <cheinsen@movableink.com>
2024-03-17 20:21:12 -0400

    Release 1.5.1

--- Package.swift
+++ Package.swift
@@ -21,8 +21,8 @@ let package = Package(
   targets: [
    .binaryTarget(
      name: "MovableInk",
-     url: "https://github.com/movableink/ios-sdk/releases/download/1.5.0/MovableInk.xcframework.zip",
-     checksum: "9567b099c4140955b6338b4765c00299bb5969b0f7e393c45466fb500e9f7831"
+     url: "https://github.com/movableink/ios-sdk/releases/download/1.5.1/MovableInk.xcframework.zip",
+     checksum: "fccc9ed93f932341a73ca8327be9409c0f9586bf284c1ed4f409c939e512b924"
    ),
   ]
 )
